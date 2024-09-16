c1bcf2b21989c3b6c7f86e4e4fffa60514752ac0
paddlehands88 <stefancorlett@hotmail.com>
2024-09-16 10:42:54 +0100

    up to lesson 594

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,16 @@
-const ProductsPage = () => {
-  return (
-    <div>ProductsPage</div>
-  )
-}
+import ProductsContainer from '@/components/products/ProductsContainer';
 
-export default ProductsPage
+async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: { layout?: string; search?: string };
+}) {
+  const layout = searchParams.layout || 'grid';
+  const search = searchParams.search || '';
+  return (
+    <>
+      <ProductsContainer layout={layout} search={search} />
+    </>
+  );
+}
+export default ProductsPage;
