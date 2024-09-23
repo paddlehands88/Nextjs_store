b6107f104027685c9f8c07799557bd3a23849b3f
paddlehands88 <stefancorlett@hotmail.com>
2024-09-23 16:54:47 +0100

    order checkout and stripe pages and functionality

---
+++ app/checkout/page.tsx
@@ -0,0 +1,40 @@
+'use client';
+import axios from 'axios';
+import { useSearchParams } from 'next/navigation';
+import React, { useCallback } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout,
+} from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+);
+
+
+export default function CheckoutPage() {
+  const searchParams = useSearchParams();
+
+  const orderId = searchParams.get('orderId');
+  const cartId = searchParams.get('cartId'); // these are taken from the URL created by the createOrderAction redirect()
+
+  const fetchClientSecret = useCallback(async () => {
+    // Create a Checkout Session
+    const response = await axios.post('/api/payment', {
+      orderId: orderId,
+      cartId: cartId,
+    });
+    return response.data.clientSecret;
+  }, []);
+
+  const options = { fetchClientSecret };
+
+  return (
+    <div id='checkout'>
+      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
+    </div>
+  );
+}
